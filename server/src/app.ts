aa14ac472379045bf8d27678f421bc8c61d23f9e
Ezra Lawrence <ezralawrence02@gmail.com>
2026-01-16 17:46:45 -0500

    feat(app): mount auth and user route handlers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import type { Application } from "express";
+import authRoute from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js"
 
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoutes);
 
 export default app;
